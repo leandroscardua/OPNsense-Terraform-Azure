3e31986489846a0f19e0f28a50fb2b0bd5a9c277
Leandro Scardua <leandroscar@gmail.com>
2023-03-23 10:55:57 +0000

    starting the build

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 #OPNsense-Terraform-Azure
+
+## If you want to test OPNsense on azure, without paying lincese fees.
+## inspired by https://github.com/dmauser/opnazure/
+
+- OPNsense On FreeBSD VM
+
+# Go to community folder
+
+## If you wnat to deploy in production or test the Official deploy.
+
+- OPNsense official release: https://docs.opnsense.org/manual/how-tos/installazure.html
+-- Marketplace: https://azuremarketplace.microsoft.com/en/marketplace/apps/decisosalesbv.opnsense?tab=Overview
+
+# Go to official folder
+
--- official/opnsense.tf
+++ official/opnsense.tf
@@ -2,7 +2,7 @@ resource "azurerm_linux_virtual_machine" "opnsense" {
   name                = "opnsense"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_F2"
+  size                = ""
   admin_username      = var.opn_user_name
   admin_password      = var.opn_user_password
   disable_password_authentication = false
@@ -10,20 +10,15 @@ resource "azurerm_linux_virtual_machine" "opnsense" {
     azurerm_network_interface.opnsense-nic.id,
   ]
 
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
-
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
+    publisher = ""
+    offer     = ""
+    sku       = ""
+    version   = ""
   }
 }
